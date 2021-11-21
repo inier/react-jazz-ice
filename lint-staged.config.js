4c5e623b9f2e0e05b5796a4d01ad6fa69d23438d
inier <inier@qq.com>
2021-11-21 23:16:23 +0800

    fix: 更新

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,9 +3,7 @@
 module.exports = {
   'src/**/*.{js,jsx}': ['eslint', 'prettier --write'],
   'src/**/*.{ts,tsx}': ['eslint', 'prettier --parser typescript --write'],
-  '**/*.css': ['stylelint', 'prettier --write'],
-  '**/*.less': ['stylelint --syntax=less', 'prettier --write'],
-  '**/*.scss': ['stylelint --syntax=scss', 'prettier --write'],
+  // '**/*.{css,scss}': ['stylelint', 'prettier --write'],
   '**/*.json': ['prettier --write'],
   '**/*.{md,markdown}': ['lint-md'],
 };
